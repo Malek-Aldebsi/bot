bda87f066b9a7f5aece6caa50d3f9def1cbf2b89
Malek-Aldebsi <malek315@gmail.com>
2024-08-07 11:36:07 +0300

    ...

---
+++ launch/rplidar.launch.py
@@ -0,0 +1,23 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_node',
+            output='screen',
+            parameters=[{
+                # ls /dev/serial/by-path
+                'serial_port': '/dev/USB0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
+
+# killall rplidar_composition
